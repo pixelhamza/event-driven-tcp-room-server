34f47dda577cd3905f08d732b590273f716e400e
pixelhamza <nashithwk25@gmail.com>
2026-07-05 02:45:05 +0530

    Read data from connected clients via poll

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,57 @@
 #include "Socket.hpp"
 #include <iostream>
+#include<poll.h>
+#include<vector>
+#include<utility>
 
 int main() {
     Socket listener;
-    listener.listenOn(8080);
     if (!listener.listenOn(8080)) {
-        std::cerr << "Failed to listen on port 8080\n";
+        std::cerr << "Failed to start server\n";
         return 1;
     }
-    std::cout<<" we listening on 8080"<<std::endl;
-    Socket client = listener.accept();
-    if(client.isValid()){
-        std::cout<<"we locked in boi";
+    std::cout<<"We listening on 8080"<<std::endl;
+    
+    std::vector<pollfd> fds;
+    pollfd listenerPfd;
+    listenerPfd.fd = listener.fd();
+    listenerPfd.events = POLLIN;
+    listenerPfd.revents = 0;
+
+    fds.push_back(listenerPfd);
+
+    std::vector<Socket> clients;
+
+    while( true ){ 
+        int ready = poll(fds.data(),fds.size(),-1);
+        if(ready < 0){
+             std::cerr << "poll failed\n";
+            break;
+
+        }
+
+        if (fds[0].revents & POLLIN) {
+            Socket client = listener.accept();
+            
+            if(client.isValid()){
+                std::cout<<"New Connection established"<<std::endl;
+                pollfd clientPfd;
+                clientPfd.fd = client.fd();
+                clientPfd.events = POLLIN;
+                clientPfd.revents = 0;
+                fds.push_back(clientPfd);
+                clients.push_back(std::move(client));
+            }
+            
+        }
+        for (size_t i = 1; i < fds.size(); ++i) {
+            if (fds[i].revents & POLLIN) {
+            std::cout << "Client at fds[" << i << "] has data ready\n";
+            }
+        }
+
+        
     }
+    return 0;
+    
 }
